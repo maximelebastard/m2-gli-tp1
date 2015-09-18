00236f86588fc312667df203fb10e6484218a2bc
romain <ramageromain@gmail.com>
2015-09-18 16:07:19 +0200

    add adapter

---
+++ src/main/java/model/ItemSetAdapter.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ItemSetAdapter extends ItemSet{
+	
+	private Observable observable = new Observable();
+	
+	public ItemSetAdapter(){
+		super();
+	}
+	
+	/**
+	 * Method to add item and to notify observers
+	 * @param item : item to add
+	 */
+	public void addItem(Item item){
+		super.addItem(item);
+	}
+	
+	/**
+	 * Method to remove item and to notify observers
+	 * @param item : item to remove
+	 */
+	public void removeItem(Item item){
+		super.removeItem(item);
+		observable.notifyObservers();
+	}
+
+
+	public void addObserver(Observer observer) {
+		observable.addObserver(observer);
+	}
+
+	public void removeObserver(Observer observer) {
+		observable.deleteObserver(observer);
+	}
+}
