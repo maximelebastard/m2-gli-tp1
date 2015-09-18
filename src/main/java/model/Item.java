a60ab0cbaa0712ca53efeb74d6126335083578ee
Maxime Lebastard <maxime.lebastard@etudiant.univ-rennes1.fr>
2015-09-18 15:28:14 +0200

    Add models

---
+++ src/main/java/model/Item.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Item {
+	/**
+	 * @var title The item title
+	 */
+	private String title;
+	
+	/**
+	 * @var description The item description
+	 */
+	private String description;
+	
+	/**
+	 * @var number The item number
+	 */
+	private Integer number;
+	
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the number
+	 */
+	public Integer getNumber() {
+		return number;
+	}
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+	
+}
---
+++ src/main/java/model/ItemSet.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author maxime
+ *
+ */
+public class ItemSet {
+	
+	/**
+	 * @var title The itemset title
+	 */
+	private String title;
+	
+	/**
+	 * @var items Items of the itemset
+	 */
+	private List<Item> items;
+
+	public ItemSet() {
+		super();
+	}
+	
+	public ItemSet(String title) {
+		super();
+		this.setTitle(title);
+		this.setItems(new ArrayList<Item>());
+	}
+	
+	public ItemSet(String title, List<Item> items) {
+		super();
+		this.setTitle(title);
+		this.setItems(items);
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the items
+	 */
+	public List<Item> getItems() {
+		return items;
+	}
+
+	/**
+	 * @param items the items to set
+	 */
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+	
+	/**
+	 * Adds a new item in the set
+	 * 
+	 * @param item The item to add
+	 */
+	public void addItem(Item item){
+		this.items.add(item);
+	}
+	
+	/**
+	 * Removes an item from the set
+	 * 
+	 * @param item The item to remove
+	 */
+	public void removeItem(Item item){
+		this.items.remove(item);
+	}
+
+}
